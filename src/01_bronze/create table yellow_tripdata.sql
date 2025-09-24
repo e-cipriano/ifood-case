0a4e4295ca019b4a410c0a17fe6b790138a2ef88
user.name like: "e-cipriano <user,mail like: "erika.cipriano@grupoboticario.com.br>
2025-09-24 10:39:54 -0300

    ajuste create bronze para databricks

--- src/01_bronze/create table yellow_tripdata.sql
+++ src/01_bronze/create table yellow_tripdata.sql
@@ -1,25 +1,23 @@
 create table yellow_tripdata (
-  VendorID                  INT64       OPTIONS(description="Código do fornecedor"),
-  tpep_pickup_datetime      TIMESTAMP   OPTIONS(description="Data e hora de início da viagem"),
-  tpep_dropoff_datetime     TIMESTAMP   OPTIONS(description="Data e hora de término da viagem"),
-  passenger_count           FLOAT64     OPTIONS(description="Número de passageiros"),
-  trip_distance             FLOAT64     OPTIONS(description="Distância da viagem em milhas"),
-  RatecodeID                FLOAT64     OPTIONS(description="Tipo de tarifa"),
-  store_and_fwd_flag        STRING      OPTIONS(description="Indica se a viagem foi armazenada e encaminhada"),
-  PULocationID              INT64       OPTIONS(description="Código do local de início da viagem"),
-  DOLocationID              INT64       OPTIONS(description="Código do local de término da viagem"),
-  payment_type              INT64       OPTIONS(description="Tipo de pagamento"),
-  fare_amount               FLOAT64     OPTIONS(description="Valor da tarifa"),
-  extra                     FLOAT64     OPTIONS(description="Taxa extra"),
-  mta_tax                   FLOAT64     OPTIONS(description="Imposto de Mobilidade para Autoridade de Transporte Metropolitano MTA"),
-  tip_amount                FLOAT64     OPTIONS(description="Valor da gorjeta"),
-  tolls_amount              FLOAT64     OPTIONS(description="Valor dos pedágios"),
-  improvement_surcharge     FLOAT64     OPTIONS(description="Taxa de melhoria"),
-  total_amount              FLOAT64    OPTIONS(description="Valor total da viagem")
+  VendorID                  INT64       ,
+  tpep_pickup_datetime      TIMESTAMP   ,
+  tpep_dropoff_datetime     TIMESTAMP   ,
+  passenger_count           FLOAT64     ,
+  trip_distance             FLOAT64     ,
+  RatecodeID                FLOAT64     ,
+  store_and_fwd_flag        STRING      ,
+  PULocationID              INT64       ,
+  DOLocationID              INT64       ,
+  payment_type              INT64       ,
+  fare_amount               FLOAT64     ,
+  extra                     FLOAT64     ,
+  mta_tax                   FLOAT64     ,
+  tip_amount                FLOAT64     ,
+  tolls_amount              FLOAT64     ,
+  improvement_surcharge     FLOAT64     ,
+  total_amount              FLOAT64    
 )
 
 PARTITION BY DATE(tpep_pickup_datetime)
 CLUSTER BY VendorID
-OPTIONS(
-  description="Dados de Viagem de Yellow Taxi em NYC"
-);
+
