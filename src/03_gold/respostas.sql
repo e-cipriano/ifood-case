723852532756efb126f6fcd11df0c41579979d86
user.name like: "e-cipriano <user,mail like: "erika.cipriano@grupoboticario.com.br>
2025-09-24 15:14:28 -0300

    respostas sql

--- src/03_gold/respostas.sql
+++ src/03_gold/respostas.sql
@@ -14,7 +14,7 @@ GROUP BY month
 -- Pergunta: Qual a média de valor total (total_amount) recebido em um mês?
 -- considerando todos os yellow táxis da frota?
 
--- 1) Média por mês (média por corrida dentro de cada mês)
+-- 1) Média por mês ( por corrida dentro de cada mês)
 SELECT
   date_format(DATE_TRUNC(tpep_pickup_datetime, MONTH), "yyyy-MM") AS month,
   COUNT(1) AS rides_count,
@@ -34,23 +34,14 @@ FROM gold.tbg_yellow_tripdata
 WHERE 
 date_format(DATE_TRUNC(tpep_pickup_datetime, MONTH), "yyyy-MM") = “2023-05”
 
-
--- 3) Média diária dentro de um mês (útil para comparar dias)
+-- Media de passageiros por hora do dia, no mes de maio de 2023
 SELECT
-  DATE(tpep_pickup_datetime) AS day,
-  COUNT(*) AS rides_count,
-  SUM(total_amount) AS sum_total_amount,
-  AVG(total_amount) AS avg_total_amount_per_ride
-FROM gold.tbg_yellow_tripdata
-WHERE DATE_TRUNC(tpep_pickup_datetime, MONTH) = DATE_TRUNC(DATE(:year || '-' || LPAD(CAST(:month AS STRING),2,'0')), MONTH)
-GROUP BY day
-ORDER BY day;
-
-
--- 4) Observações / data quality
--- - Recomenda-se filtrar/outlier por valores negativos ou durations inválidas antes de calcular médias.
--- - Exemplo de filtro para remover valores inválidos:
---   WHERE total_amount >= 0
---     AND tpep_dropoff_datetime >= tpep_pickup_datetime
+    tpep_pickup_hour,
+    AVG(passenger_count) AS avg_passenger_count
+FROM gold.tbg_yellow_tripdata0
+WHERE 
+    date_format(tpep_pickup_datetime, "yyyy-MM") = "2023-05"
+GROUP BY tpep_pickup_hour
+ORDER BY tpep_pickup_hour;  
 
 -- Fim
